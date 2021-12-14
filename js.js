becfe1e40d9c276822124e72a924b514be9d823b
Mohammed <mohd.ntw@gmail.com>
2021-12-13 20:28:48 -0700

    done

---
+++ js.js
@@ -0,0 +1,28 @@
+for (let i = 1 ; i < 10 ; i++ ){
+    if (i % 2 !== 0){
+        console.log(i)
+    }
+}
+
+for (let i = 100 ; i >= 0 ; i--){
+    if (i % 3 === 0){
+        console.log(i)
+    }
+}
+
+let sum = 0 ;
+for (let i = 0 ; i <= 100 ; i++){
+    sum += i
+
+
+}
+
+console.log(sum)
+let product = 1
+for (let i = 1 ; i <= 12 ; i++){
+    product *= i
+
+
+}
+
+console.log(product)
